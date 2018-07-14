80f46ba912ec09b5241d48c27f229972260f5f62
Misha <ishay.fik@gmail.com>
2018-07-14 17:11:35 +0300

    Added n model and controller of branch

---
+++ app/controllers/branches/branchesController.js
@@ -0,0 +1,124 @@
+'use strict';
+
+define(['app'], function (app) {
+
+    var branchesController = function ($scope, $location, $filter, dataService, modalService) {
+
+        $scope.branches = [];
+        $scope.filteredBranches = [];
+        $scope.filteredCount = 0;
+        $scope.orderby = 'name';
+        $scope.reverse = false;
+
+        //paging
+        $scope.totalRecords = 0;
+        $scope.pageSize = 10;
+        $scope.currentPage = 1;
+
+        init();
+
+        $scope.pageChanged = function (page) {
+            $scope.currentPage = page;
+            getBranchesSummary();
+        };
+
+        $scope.deleteBranch = function (id) {
+            var branch = getBranchById(id);
+            var branchName = branch.name;
+
+            var modalOptions = {
+                closeButtonText: 'Cancel',
+                actionButtonText: 'Delete Branch',
+                headerText: 'Delete ' + branchName + '?',
+                bodyText: 'Are you sure you want to delete this branch?'
+            };
+
+            modalService.showModal({}, modalOptions).then(function (result) {
+                if (result === 'ok') {
+                    dataService.deleteBranch(id).then(function () {
+                        for (var i = 0; i < $scope.branches.length; i++) {
+                            if ($scope.branches[i].id == id) {
+                                $scope.branches.splice(i, 1);
+                                break;
+                            }
+                        }
+                        filterBranches($scope.searchText);
+                    }, function (error) {
+                        alert('Error deleting branch: ' + error.message);
+                    });
+                }
+            });
+        };
+
+        $scope.ViewEnum = {
+            Card: 0,
+            List: 1
+        }
+
+        $scope.changeView = function (view) {
+            switch (view) {
+                case $scope.ViewEnum.Card:
+                    $scope.listViewEnabled = false;
+                    break;
+                case $scope.ViewEnum.List:
+                    $scope.listViewEnabled = true;
+                    break;
+            }
+        }
+
+        $scope.navigate = function (url) {
+            $location.path(url);
+        }
+
+        $scope.setOrder = function (orderby) {
+            if (orderby === $scope.orderby) {
+                $scope.reverse = !$scope.reverse;
+            }
+            $scope.orderby = orderby;
+        };
+
+        function init() {
+            createWatches();
+            getBranchesSummary();
+        }
+
+        function createWatches() {
+            //Watch searchText value and pass it and the branches to nameCityStateFilter
+            //Doing this instead of adding the filter to ng-repeat allows it to only be run once (rather than twice)
+            //while also accessing the filtered count via $scope.filteredCount above
+            $scope.$watch("searchText", function (filterText) {
+                filterBranches(filterText);
+            });
+        }
+
+        function getBranchesSummary() {
+            dataService.getBranchesSummary($scope.currentPage - 1, $scope.pageSize)
+                .then(function (data) {
+                    $scope.totalRecords = data.totalRecords;
+                    $scope.branches = data.results;
+                    filterBranches(''); //Trigger initial filter
+                }, function (error) {
+                    alert(error.message);
+                });
+        }
+
+        function filterBranches(filterText) {
+            $scope.filteredBranches = $filter("nameCityStateFilter")($scope.branches, filterText);
+            $scope.filteredCount = $scope.filteredBranches.length;
+        }
+
+        function getBranchById(id) {
+            for (var i = 0; i < $scope.branches.length; i++) {
+                var cust = $scope.branches[i];
+                if (cust.id === id) {
+                    return cust;
+                }
+            }
+        }
+
+    };
+
+    app.register.controller('BranchesController',
+        ['$scope', '$location', '$filter', 'dataService', 'modalService', branchesController]);
+
+});
---
+++ server/models/branch.js
@@ -0,0 +1,53 @@
+var mongoose = require('mongoose')
+    , Schema = mongoose.Schema
+    , ObjectId = Schema.ObjectId;
+
+
+var SettingsSchema = new Schema({
+    collectionName : {
+        type : String, required: true, trim: true, default: 'Branches'
+    },
+    nextSeqNumber: {
+        type: Number, default: 1
+    }
+});
+
+var BranchSchema = new Schema({
+    name : {
+        type : String, required: true, trim: true
+    },
+    phone : {
+        type : String, required: true, trim: true
+    },
+    openingHours : {
+        type : String, required: true, trim: true
+    },
+    longitude : {
+        type : Number, required: true, trim: true
+    },
+    latitude : {
+        type : String, required: true, trim: true
+    },
+});
+
+BranchSchema.index({ id: 1, type: 1 }); // schema level
+
+// I make sure this is the last pre-save middleware (just in case)
+var Settings = mongoose.model('settings', SettingsSchema);
+
+BranchSchema.pre('save', function(next) {
+    var doc = this;
+    // Calculate the next id on new branches only.
+    if (this.isNew) {
+        Settings.findOneAndUpdate( {"collectionName": "Branch"}, { $inc: { nextSeqNumber: 1 } }, function (err, settings) {
+            if (err) next(err);
+            doc.id = settings.nextSeqNumber - 1; // substract 1 because I need the 'current' sequence number, not the next
+            next();
+        });
+    } else {
+        next();
+    }
+});
+
+exports.BranchSchema = BranchSchema;
+module.exports = mongoose.model('Users', BranchSchema);
