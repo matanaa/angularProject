75679b841acc3631d3446666a7b912c8d52f81db
Misha <ishay.fik@gmail.com>
2018-07-14 20:13:38 +0300

    Added n model and controller of branch

--- server/models/branch.js
+++ server/models/branch.js
@@ -3,14 +3,14 @@ var mongoose = require('mongoose')
     , ObjectId = Schema.ObjectId;
 
 
-var SettingsSchema = new Schema({
-    collectionName : {
-        type : String, required: true, trim: true, default: 'Branches'
-    },
-    nextSeqNumber: {
-        type: Number, default: 1
-    }
-});
+//var SettingsSchema = new Schema({
+//    collectionName : {
+//        type : String, required: true, trim: true, default: 'Branches'
+//    },
+//    nextSeqNumber: {
+//        type: Number, default: 1
+//    }
+//});
 
 var BranchSchema = new Schema({
     name : {
@@ -33,13 +33,13 @@ var BranchSchema = new Schema({
 BranchSchema.index({ id: 1, type: 1 }); // schema level
 
 // I make sure this is the last pre-save middleware (just in case)
-var Settings = mongoose.model('settings', SettingsSchema);
+//var Settings = mongoose.model('settings', SettingsSchema);
 
 BranchSchema.pre('save', function(next) {
     var doc = this;
     // Calculate the next id on new branches only.
     if (this.isNew) {
-        Settings.findOneAndUpdate( {"collectionName": "Branch"}, { $inc: { nextSeqNumber: 1 } }, function (err, settings) {
+        Settings.findOneAndUpdate( {"collectionName": "Branches"}, { $inc: { nextSeqNumber: 1 } }, function (err, settings) {
             if (err) next(err);
             doc.id = settings.nextSeqNumber - 1; // substract 1 because I need the 'current' sequence number, not the next
             next();
