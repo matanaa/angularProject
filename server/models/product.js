a6d2580a1d47a427ba55175d650542a138475556
Matan <matanel@gmail.com>
2018-08-01 00:44:18 +0300

    add product id

--- server/models/product.js
+++ server/models/product.js
@@ -2,15 +2,14 @@ var mongoose = require('mongoose')
   , Schema = mongoose.Schema
   , ObjectId = Schema.ObjectId;
 
-//
-// var SettingsSchema = new Schema({
-//   collectionName : {
-//     type : String, required: true, trim: true, default: 'Users'
-//   },
-//   nextSeqNumber: {
-//     type: Number, default: 1
-//   }
-// });
+var productSettingsSchema = new Schema({
+    collectionName : {
+        type : String, required: true, trim: true, default: 'Products'
+    },
+    nextSeqNumber: {
+        type: Number, default: 1
+    }
+});
 
 
 var imagesSchema = new Schema({
@@ -24,7 +23,9 @@ var ProductSchema = new Schema({
   Name : {
     type : String, required: true, trim: true
   },
-  price : {
+    id : {
+        type : Number, required: true, unique: true
+    },  price : {
     type : Number, required: true, trim: true
   },
   type : {
@@ -39,10 +40,24 @@ var ProductSchema = new Schema({
   images: [imagesSchema],
 });
 
-//CustomerSchema.index({ id: 1, type: 1 }); // schema level
+ProductSchema.index({ id: 1, type: 1 }); // schema level
 
 // I make sure this is the last pre-save middleware (just in case)
-//var Settings = mongoose.model('settings', SettingsSchema);
+var Settings = mongoose.model('Settings', productSettingsSchema);
+
+ProductSchema.pre('validate', function(next) {
+    var doc = this;
+    // Calculate the next id on new Customers only.
+    if (this.isNew) {
+        Settings.findOneAndUpdate( {"collectionName": "Products"}, { $inc: { nextSeqNumber: 1 } }, function (err, settings) {
+            if (err) next(err);
+            doc.id = settings.nextSeqNumber - 1; // substract 1 because I need the 'current' sequence number, not the next
+            next();
+        });
+    } else {
+        next();
+    }
+});
 
 exports.Product = ProductSchema;
 module.exports = mongoose.model('Products', ProductSchema);
